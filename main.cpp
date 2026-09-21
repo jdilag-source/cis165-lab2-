d92ca9473bc29c4cfc40e9b08ff9c860a8eb5bb9
jdilag-source <jdilag@me.bergen.edu>
2026-09-21 14:29:57 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello World\n";
+    int num1= 90;
+    int num2= 60;
+    int sum= num1+num2;
+    std::cout<<"The sum is : "<<sum<<"\n";
+
+    return 0;
+}
