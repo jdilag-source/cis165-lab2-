2d38cda3810a78918f43e66adfa4f6958dc639bc
jdilag-source <jdilag@me.bergen.edu>
2026-09-23 17:26:53 -0400

    Add MPG calculation program in mgp.cpp

---
+++ mgp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout<<"Miles per Gallon\n";
+    int num1= 312;
+    int num2= 16;
+    int mpg= num1/num2;
+    std::cout<<"The mpg is :"<<mpg<<"\n";
+
+    return 0;
+}
